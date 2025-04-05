89fb9fc47e8c93b12b701db52b6b129211a5d690
Brendan Ho <brendanhwz24@gmail.com>
2025-04-05 16:31:38 +0800

    20250331 Commit for controller url template variable fix.

--- Backend/src/main/java/net_javaguides/ems/controller/EmployeeController.java
+++ Backend/src/main/java/net_javaguides/ems/controller/EmployeeController.java
@@ -24,7 +24,7 @@ public class EmployeeController {
     }
 
     //REST API (2) -> Retrieving single employee by Id (PK)
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public ResponseEntity <EmployeeDto> getEmployeeById (@PathVariable("id") Long employeeId) {
         EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok(employeeDto);
@@ -38,7 +38,7 @@ public class EmployeeController {
     }
 
     //REST API (4) -> Updating existing employee record
-    @PutMapping("{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<EmployeeDto> updateEmployee (@PathVariable("id") Long employeeId,
                                                        @RequestBody EmployeeDto updatedEmployee) {
         EmployeeDto updateEmployeeNew = employeeService.updateEmployee(employeeId, updatedEmployee);
@@ -46,7 +46,7 @@ public class EmployeeController {
     }
 
     //REST API (5) -> Delete single employee record from DB
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteEmployee (@PathVariable("id") Long employeeId) {
         employeeService.deleteEmployee(employeeId);
         return ResponseEntity.ok("Employee is successfully deleted from the records!");
